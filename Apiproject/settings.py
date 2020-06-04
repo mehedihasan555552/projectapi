fb3cdea0eca45ddee1069b5a084c588b0d6d724a
Mehedi Hasan <43444174+mehedihasan555552@users.noreply.github.com>
2020-06-05 05:23:31 +0600

    Update settings.py

--- Apiproject/settings.py
+++ Apiproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a$al#sly-2+p7(d&8*h%tx4s1-bf2v26yw4p#x@!t7&0y+3y@s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['161.35.1.234','127.0.0.1']
+ALLOWED_HOSTS = ['161.35.1.234']
 
 
 # Application definition
