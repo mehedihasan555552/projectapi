c78edd7314f882c6e7b08a9507aa714f266fe74b
Mehedi Hasan <43444174+mehedihasan555552@users.noreply.github.com>
2020-06-05 05:54:05 +0600

    Update settings.py

--- Apiproject/settings.py
+++ Apiproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a$al#sly-2+p7(d&8*h%tx4s1-bf2v26yw4p#x@!t7&0y+3y@s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['161.35.1.234']
 
