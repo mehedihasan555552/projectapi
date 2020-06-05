770a96d80db76202dc628408dd3057b73c9f56d9
Mehedi Hasan <43444174+mehedihasan555552@users.noreply.github.com>
2020-06-05 06:20:17 +0600

    Update settings.py

--- Apiproject/settings.py
+++ Apiproject/settings.py
@@ -152,4 +152,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/images/'
 
+STATICFILES_DIRS =[
+   os.path.join(BASE_DIR,'static')
+]
 
+MEDIA_ROOT= os.path.join(BASE_DIR,'static/images')
